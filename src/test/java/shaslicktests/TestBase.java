5117edf379719b7f1a0f4b5e6d6745b72eed0834
Misha_Vib <nsutkov382@gmail.com>
2022-05-16 01:54:45 +0300

    fix

--- src/test/java/shaslicktests/TestBase.java
+++ src/test/java/shaslicktests/TestBase.java
@@ -3,6 +3,10 @@ package shaslicktests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.ProjectProvider;
+import helpers.AllureAttach;
+import helpers.DriverSettings;
+import helpers.DriverUtils;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,26 +16,33 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Selenide.clearBrowserCookies();
-        Configuration.baseUrl = "https://shashlik.club/";
-        Configuration.browserSize = "1920x1080";
+       // Configuration.baseUrl = "https://shashlik.club/";
+        //Configuration.browserSize = "1920x1080";
         SelenideLogger.addListener("allure", new AllureSelenide());
+        DriverSettings.configure();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
+       // DesiredCapabilities capabilities = new DesiredCapabilities();
+       // capabilities.setCapability("enableVNC", true);
+       // capabilities.setCapability("enableVideo", true);
+       // Configuration.browserCapabilities = capabilities;
 
-        String login = System.getProperty("login");
-        String password = System.getProperty("password");
-        Configuration.remote = "https://" + login + ":" + password + "@" + System.getProperty("remoteBrowser");
+       // String login = System.getProperty("login");
+       // String password = System.getProperty("password");
+       // Configuration.remote = "https://" + login + ":" + password + "@" + System.getProperty("remoteBrowser");
     }
 
-@AfterEach
-void addAttachments() {
-    AttachAllure.screenshotAs("Last screenshot");
-    AttachAllure.pageSource();
-    AttachAllure.browserConsoleLogs();
-    AttachAllure.addVideo();
-    Selenide.closeWebDriver();
-}
+    @AfterEach
+    public void afterTest() {
+
+        String sessionId = DriverUtils.getSessionId();
+
+        AllureAttach.addScreenshotAs("Last screenshot");
+        AllureAttach.addPageSource();
+        AllureAttach.addBrowserConsoleLogs();
+        Selenide.closeWebDriver();
+
+        if (ProjectProvider.isVideoOn()) {
+            AllureAttach.addVideo(sessionId);
+        }
+    }
 }
