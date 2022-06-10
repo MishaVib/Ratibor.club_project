e74d0f13e7163fca57ea14a2289a613ed42b231c
Misha_Vib <nsutkov382@gmail.com>
2022-06-10 23:06:37 +0300

    Поправил автотесты раздела Бронирование

--- src/test/java/shaslicktests/Booking.java
+++ src/test/java/shaslicktests/Booking.java
@@ -1,17 +1,19 @@
 package shaslicktests;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.logevents.SelenideLogger;
+
+import com.codeborne.selenide.Selenide;
 import io.qameta.allure.*;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 @Tag("regress")
-public class Booking extends TestBase{
+public class Booking extends TestBase {
     @Test
     @Owner("Никита Шутков")
     @Severity(SeverityLevel.NORMAL)
@@ -21,20 +23,39 @@ public class Booking extends TestBase{
     @Feature("Бронь")
     void bookingTable() {
         step("Открываем главную страницу", () -> {
-        open("https://shashlik.club");
+            open("https://shashlik.club");
         });
         step("Клик на кнопку Забронировать стол", () -> {
-        $(byText("Забронировать стол")).click();
+            $(byText("Забронировать стол")).click();
         });
-        step("Проверка текста в разделе бронирования стола", () -> {
-        $(".site-main").shouldHave(Condition.text("Забронировать стол"));
-        $(".site-main").shouldHave(Condition.text("Забронировать стол можно по телефону"));
-        $x("//a[@href='tel:+79302220032']").shouldHave(Condition.text("+7 (930) 222 00 32"))
-                .shouldBe(Condition.visible);
-        $(".site-info").shouldHave(Condition.text(" Просто качественная доставка еды ©"))
-                .shouldBe(Condition.visible);
-        $x("//a[@href='https://original-watch.online/']").shouldHave(Condition.exactText("продать часы"))
-                .shouldBe(Condition.visible);
+        step("Проверка элементов в разделе бронирования стола", () -> {
+            $("h1").shouldHave(text("Забронировать стол")).shouldBe(visible);
+            $("main p").shouldHave(text("Забронировать стол можно по телефону")).$("a")
+                    .shouldHave(text("+7 (930) 222 00 32"));
+
+            $(".site-info").shouldHave(text(" Просто качественная доставка еды ©")).shouldBe(visible)
+                    .$("a").shouldHave(text("Оригинальные швейцарские часы")).shouldBe(visible)
+                    .shouldHave(attributeMatching("href", "https://original-watches.ru/"));
+        });
+    }
+
+    @Test
+    @Owner("Никита Шутков")
+    @Severity(SeverityLevel.MINOR)
+    @Description(
+            "Переход по ссылке на сайт Оригинальные Швейцарские часы"
+    )
+    @Feature("Ссылка")
+    void deeplink() {
+        step("Открыть страницу Забронировать стол", () -> {
+            Selenide.open("https://shashlik.club/sample-page/");
+        });
+        step("Клик по ссылке", () -> {
+            $(byText("Оригинальные швейцарские часы")).click();
+        });
+        step("Проверка, что открылся сайт", () -> {
+            $("h1").shouldHave(exactText("Оригинальные Швейцарские часы"));
         });
     }
 }
+
