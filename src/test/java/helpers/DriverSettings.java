54510c14aac9ea73f70c92c540fcdbc633f605d7
Misha_Vib <nsutkov382@gmail.com>
2022-06-29 01:02:29 +0300

    improved

--- src/test/java/helpers/DriverSettings.java
+++ src/test/java/helpers/DriverSettings.java
@@ -14,6 +14,7 @@ public class DriverSettings {
         Configuration.browserVersion = Project.config.browserVersion();
         Configuration.browserSize = Project.config.browserSize();
         Configuration.timeout = Project.config.timeout();
+        Configuration.baseUrl = System.getProperty("https://shashlik.club/");
 
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
